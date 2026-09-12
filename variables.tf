fe8805a61515129ad263cd6d4a5e2d3958fa8c9d
Srinivasa Phanindra V <sphanindra@gmail.com>
2026-09-12 16:38:45 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -30,3 +30,38 @@ variable "public_subnet_tags" {
     type = map
     default = {}
 }
+
+variable "private_subnet_cidrs" {
+  type = list 
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "private_subnet_tags" {
+    type = map
+    default = {}
+}
+
+variable "database_subnet_cidrs" {
+  type = list 
+  default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
+variable "database_subnet_tags" {
+    type = map
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    type = map
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    type = map
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    type = map
+    default = {}
+}
