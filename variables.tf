3e12c7bcbf88ed55ed03a1980d1b0dec784bd1c4
Srinivasa Phanindra V <sphanindra@gmail.com>
2026-09-12 17:18:28 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -65,3 +65,13 @@ variable "database_route_table_tags" {
     type = map
     default = {}
 }
+
+variable "eip_tags" {
+    type = map
+    default = {}
+}
+
+variable "nat_gateway_tags" {
+    type = map
+    default = {}
+}
