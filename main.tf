62cf50cddff591b2a2b20f4f117df327bfe56411
Srinivasa Phanindra V <sphanindra@gmail.com>
2026-09-12 16:41:10 +0530

    terraform

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "aws_subnet" "database" {
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.example.id
+  vpc_id = aws_vpc.main.id
 
   tags = merge(
         local.common_tags,
@@ -78,7 +78,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.example.id
+  vpc_id = aws_vpc.main.id
 
   tags = merge(
         local.common_tags,
@@ -91,7 +91,7 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table" "database" {
-  vpc_id = aws_vpc.example.id
+  vpc_id = aws_vpc.main.id
 
   tags = merge(
         local.common_tags,
